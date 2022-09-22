4a7816a4d7c3f6096fe405256ca7445c5bdd8a07
Myroslav Volkov <miroslav.volkov@gmail.com>
2022-09-22 21:29:28 +0300

    battery -> as reactive entity Dock/Undock button

--- Components/Sources/Components/ControlPanel/ButtonDockViewModel.swift
+++ Components/Sources/Components/ControlPanel/ButtonDockViewModel.swift
@@ -1,37 +1,60 @@
+import Combine
+import Connection
 import Features
 import SwiftUI
 
 class ButtonDockViewModel: ControlPanelButtonViewModel {
-    @Published var isLoading: Bool = false
     @Published var enabled: Bool = true
     @Published var primaryIcon: Image?
     @Published var primaryTitle: String?
     @Published var secondaryTitle: String?
     @Published var tintColor: Color = .green
+
     private let connection: ConnectionModel
+    private var bag = Set<AnyCancellable>()
+    private var battery: VectorBatteryState? {
+        didSet {
+            switch battery {
+            case .charging, .full:
+                primaryIcon = Image(systemName: "square.and.arrow.up")
+            default:
+                primaryIcon = Image(systemName: "square.and.arrow.down")
+            }
+        }
+    }
 
     init(connection: ConnectionModel) {
         self.connection = connection
         self.primaryIcon = .init(systemName: "square.and.arrow.down")
     }
-    
+
     func bind() {
         Task {
-            if try await connection.battery == .charging {
-                self.primaryIcon = .init(systemName: "square.and.arrow.up")
-            } else {
-                self.primaryIcon = .init(systemName: "square.and.arrow.down")
-            }
+            await connection.battery
+                .subscribe(on: RunLoop.main)
+                .compactMap { $0 }
+                .assign(to: \.battery, on: self)
+                .store(in: &bag)
         }
     }
-    
+
+    func unbind() {
+        bag.removeAll()
+    }
+
     func onClick() {
         Task {
-            print(await try connection.battery)
-            if try await connection.battery == .charging {
+            guard let battery = battery else {
+                return
+            }
+
+            switch battery {
+            case .full, .charging:
                 try await connection.undock()
-            } else {
+            case .normal, .low:
                 try await connection.dock()
+            default:
+                break
             }
         }
     }
--- Features/Sources/Features/Connection/ConnectionModel.swift
+++ Features/Sources/Features/Connection/ConnectionModel.swift
@@ -5,33 +5,20 @@ import os.log
 
 public final actor ConnectionModel {
     typealias VectorDevice = Connection & Behavior & Camera & Audio
-
+    
     public enum ConnectionModelState {
         case disconnected
         case connecting
         case online
     }
-
-    /// Vector's battery condition property
-    /// - Description battery conditions are FULL/NORMAL/LOW/CHARGING
-    /// - Returns battery state asyhc value
-    public var battery: VectorBatteryState {
-        get async throws {
-            guard let battery = try? await connection?.battery else {
-                throw ConnectionError.notConnected
-            }
-
-            return battery
-        }
-    }
-
+  
     /// Vector behavior API access
     /// - Description nil means vector is not connected
     /// - Returns Behavior optional type entity
     public var behavior: Behavior? {
         connection
     }
-
+    
     /// Vector camera feed access
     /// - Description grants camera feed
     /// - Returns optinal AsyncStream with camera feed
@@ -40,7 +27,7 @@ public final actor ConnectionModel {
             try connection?.requestCameraFeed()
         }
     }
-
+    
     /// Vector microphone feed access
     /// - Description grants microphone feed
     /// - Returns optinal AsyncStream with microphone feed
@@ -49,32 +36,38 @@ public final actor ConnectionModel {
             try connection?.requestMicFeed()
         }
     }
-
+    
     /// Vector's connection state reactive property
     public var state: CurrentValueSubject<ConnectionModelState, Never> = .init(.disconnected)
-
+    
     /// Vector's robot state reactive property
     public var robotState: PassthroughSubject<Anki_Vector_ExternalInterface_RobotState, Never> = .init()
-
+    
+    /// Vector's battery state reactive property
+    public var battery: PassthroughSubject<VectorBatteryState, Never> = .init()
+    
     private var connection: VectorDevice?
     private var bag = Set<AnyCancellable>()
     private let logger = Logger(subsystem: "com.mirfirstsnow.ivector", category: "main")
     private lazy var tts = TextToSpeech()
-
-    public init() {}
-
+    @MainActor private var timer: Timer?
+    
+    public init() {
+        Task { await self.bind() }
+    }
+    
     public func bind() {
         state
             .removeDuplicates(by: { $0 == $1 })
             .sink(receiveValue: { state in self.process(state: state) })
             .store(in: &bag)
     }
-
+    
     public func connect(with ipAddress: String, port: Int = 443) {
         guard case .disconnected = state.value else {
             return
         }
-
+        
         connection = VectorConnection(with: ipAddress, port: port)
         connection?.delegate = self
         do {
@@ -85,7 +78,7 @@ public final actor ConnectionModel {
             state.send(.disconnected)
         }
     }
-
+    
     public func disconnect() {
         do {
             try connection?.release()
@@ -95,35 +88,50 @@ public final actor ConnectionModel {
             state.send(.disconnected)
         }
     }
-
+    
     /// Says text with vector speaker hardware
     public func say(text: String, locale: Locale = .current) throws {
         let stream = tts.run(text, locale: locale)
         try connection?.playAudio(stream: stream)
     }
-
-    /// Plays wav file 
+    
+    /// Plays wav file
     public func play(name: SoundPlayer.SoundName) throws {
         let player = SoundPlayer()
         let stream = player.play(name: name)
         try connection?.playAudio(stream: stream)
     }
-
+    
     private func process(state: ConnectionModelState) {
         switch state {
         case .online:
             Task.detached { try await self.onConnected() }
-
+            
         case .connecting:
             logger.debug("connecting...")
-
+            
         case .disconnected:
-            break
+            Task.detached { try await self.onDisconnected() }
         }
     }
-
-    private func onConnected() throws {
+    
+    private func onConnected() async throws {
         try connection?.requestEventStream()
+        await MainActor.run {
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+                Task {
+                    if let battery = try? await self.connection?.battery {
+                        await self.battery.send(battery)
+                    }
+                }
+            })
+        }
+    }
+    
+    private func onDisconnected() async throws {
+        await MainActor.run(body: {
+            timer?.invalidate()
+        })
     }
 }
 
@@ -138,7 +146,7 @@ extension ConnectionModel: ConnectionDelegate {
 
     public nonisolated func keepAlive() {
         Task.detached {
-            if await self.state.value != .online {
+            if await self.state.value == .disconnected {
                 await self.state.send(.connecting)
             }
         }
