1e3a2370a06d28ebdd92fc496a914633dd8b580e
Myroslav Volkov <miroslav.volkov@gmail.com>
2023-09-20 14:43:27 +0300

    set macOS supported os to 13

--- Features/Package.swift
+++ Features/Package.swift
@@ -18,7 +18,7 @@ let resources: [Resource] = [
 
 let package = Package(
     name: "Features",
-    platforms: [.macOS("11.0.0"), .iOS("16.0")],
+    platforms: [.macOS("13.0.0"), .iOS("16.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
